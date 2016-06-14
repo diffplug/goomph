772a9543d8d0cbcb6dacc74da14e40de8b66b179
Ned Twigg <ned.twigg@diffplug.com>
2016-06-14 03:20:16 -0700

    Added BootstrapInstallation, which can download an installation from bintray's goomph-p2-bootstrap.

---
+++ src/main/java/com/diffplug/gradle/p2/P2BootstrapInstallation.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.gradle.p2;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+import org.apache.commons.io.FileUtils;
+
+import com.diffplug.gradle.FileMisc;
+import com.diffplug.gradle.GoomphCacheLocations;
+import com.diffplug.gradle.ZipUtil;
+import com.diffplug.gradle.eclipse.EclipseRelease;
+
+/** Wraps a Bootstrap installation for the given eclipse release. */
+class P2BootstrapInstallation {
+	static final String DOWNLOAD_ROOT = "https://dl.bintray.com/diffplug/opensource/com/diffplug/gradle/goomph-p2-bootstrap/";
+	static final String DOWNLOAD_FILE = "/goomph-p2-bootstrap.zip";
+
+	final EclipseRelease release;
+
+	public P2BootstrapInstallation(EclipseRelease release) {
+		this.release = Objects.requireNonNull(release);
+	}
+
+	/** The root of this installation. */
+	public File getRootFolder() {
+		return new File(GoomphCacheLocations.p2bootstrap(), release.version());
+	}
+
+	static final String TOKEN = "installed";
+
+	/** Returns true iff it is installed. */
+	public boolean ready() throws IOException {
+		return FileMisc.hasToken(getRootFolder(), TOKEN);
+	}
+
+	/** Installs the bootstrap installation. */
+	public void install() throws IOException {
+		// clean the install folder
+		FileMisc.cleanDir(getRootFolder());
+		// download the URL
+		File target = new File(getRootFolder(), DOWNLOAD_FILE);
+		URL url = new URL(DOWNLOAD_ROOT + release.version() + DOWNLOAD_FILE);
+		FileUtils.copyURLToFile(url, target);
+		// unzip it
+		ZipUtil.unzip(target, target.getParentFile());
+		// delete the zip
+		target.delete();
+		FileMisc.writeToken(getRootFolder(), TOKEN);
+	}
+
+	/** Makes sure that the installation is prepared. */
+	public void ensureInstalled() throws IOException {
+		if (!ready()) {
+			install();
+		}
+	}
+
+	/**
+	 * Creates a model containing p2-director for the given {@link EclipseRelease}.
+	 *
+	 * Useful for updating [bintray](https://bintray.com/diffplug/opensource/goomph-p2-bootstrap/view).
+	 */
+	static P2DirectorModel createNew(EclipseRelease release) {
+		P2DirectorModel model = new P2DirectorModel();
+		// the update site for the release we're downloading artifacts for
+		model.addRepo(release.updateSite());
+		// the p2 director application and its dependencies
+		model.addIU("org.eclipse.equinox.p2.director.app");
+		model.addFeature("org.eclipse.equinox.p2.core.feature");
+		// failed transitive from p2
+		model.addIU("org.eclipse.core.net");
+		// eclipse infrastructure to make "eclipsec -application org.eclipse.equinox.p2.director" work
+		model.addIU("org.eclipse.core.runtime");
+		model.addIU("org.eclipse.update.configurator");
+		model.addIU("org.eclipse.equinox.ds");
+		return model;
+	}
+}
