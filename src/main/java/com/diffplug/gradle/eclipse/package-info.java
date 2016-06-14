524ec38ed1c698210f613a077da8bb3505b5dbad
Ned Twigg <ned.twigg@diffplug.com>
2016-06-14 03:39:01 -0700

    Added EquinoxLauncher, which enables launching a manipulating the runtime of an eclipse instance.

--- src/main/java/com/diffplug/gradle/eclipse/package-info.java
+++ src/main/java/com/diffplug/gradle/eclipse/package-info.java
@@ -1,4 +1,5 @@
 /**
- * Plugins for creating and manipulating eclipse project files.
+ * Plugins for creating and manipulating eclipse project files, as well as
+ * infrastructure for running eclipse instances.
  */
 package com.diffplug.gradle.eclipse;
