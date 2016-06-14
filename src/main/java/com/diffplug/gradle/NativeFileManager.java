ee863847fb3ea196021a7dfa533d1f135fe5390b
Ned Twigg <ned.twigg@diffplug.com>
2016-06-14 02:52:09 -0700

    Added NativeFileManager, a utility for opening the native file manager to look at a file (helpful for debugging).

---
+++ src/main/java/com/diffplug/gradle/NativeFileManager.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.gradle;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.diffplug.common.swt.os.OS;
+
+/** Interface for the native file manager. */
+public class NativeFileManager {
+	/** Opens a file manager for the given file. */
+	public static void open(File file) {
+		try {
+			String template = OS.getNative().winMacLinux("explorer /select,%s", "open %s", "xdg-open %s");
+			String cmd = template.replace("%s", quote(file));
+			CmdLine.runCmd(cmd);
+		} catch (Exception e) {
+			System.err.println("Unable to open folder in file manager: " + e.getMessage());
+		}
+	}
+
+	/** Returns the file's absolute path, quoted if necessary. */
+	private static String quote(File file) throws IOException {
+		return "\"" + file.getCanonicalPath() + "\"";
+	}
+}
