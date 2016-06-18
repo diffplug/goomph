de9ab51025a55504322e18101cdd66a2644658d9
Ned Twigg <ned.twigg@diffplug.com>
2016-06-18 00:30:11 -0700

    Added a test for MavenRepoBuilder.

---
+++ src/test/java/com/diffplug/gradle/p2/MavenRepoBuilderTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.gradle.p2;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+
+import org.gradle.internal.impldep.org.junit.Assert;
+import org.junit.Test;
+
+import com.diffplug.common.io.Files;
+import com.diffplug.common.io.Resources;
+import com.diffplug.gradle.GradleIntegrationTest;
+
+public class MavenRepoBuilderTest extends GradleIntegrationTest {
+	@Test
+	public void doTest() throws Exception {
+		File bin = copyIntoFolder("org.eclipse.ecf.provider.filetransfer.ssl_1.0.0.v20151130-0157.jar");
+		File source = copyIntoFolder("org.eclipse.ecf.provider.filetransfer.ssl.source_1.0.0.v20151130-0157.jar");
+
+		File mavenRoot = new File(folder.getRoot(), "maven");
+		try (MavenRepoBuilder builder = new MavenRepoBuilder(mavenRoot)) {
+			builder.install("p2group", bin);
+			builder.install("p2group", source);
+		}
+		write("build.gradle",
+				"apply plugin: 'java'",
+				"repositories { maven { url '" + mavenRoot.getAbsolutePath().replace("\\", "/") + "' } }",
+				"dependencies {",
+				"    compile 'p2group:org.eclipse.ecf.provider.filetransfer.ssl:1.0.0.v20151130-0157'",
+				"}",
+				"apply plugin: 'eclipse'");
+		gradleRunner().withArguments("eclipse").build();
+		String classpath = read(".classpath");
+		Assert.assertTrue(classpath.contains("org.eclipse.ecf.provider.filetransfer.ssl-1.0.0.v20151130-0157.jar\""));
+		Assert.assertTrue(classpath.contains("org.eclipse.ecf.provider.filetransfer.ssl-1.0.0.v20151130-0157-sources.jar\""));
+	}
+
+	private File copyIntoFolder(String jar) throws IOException {
+		URL url = MavenRepoBuilderTest.class.getResource(jar);
+		File file = folder.newFile();
+		try (OutputStream output = Files.asByteSink(file).openBufferedStream()) {
+			Resources.copy(url, output);
+		}
+		return file;
+	}
+}
