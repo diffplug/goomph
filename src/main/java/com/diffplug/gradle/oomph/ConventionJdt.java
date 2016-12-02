diffplug/goomph

commit 7434506f7fd3a069f899c1d535eaf0a876306c6f

Moved new JdtConvention actions to be local, where possible.
When a DSL method gets called (e.g. compilerComplicanceLevel), ideally we would call extension.workspaceProp() or extension.addSetupAction() right there, immediately.  That keeps everything local, so there are fewer places to debug.

There are cases, such as installedJres, where you have aggregate several calls to a method into a single action.  In that case, we can use the `close()` method to know that the user has finished calling methods, and we can now take our aggregated actions.

src/main/java/com/diffplug/gradle/oomph/ConventionJdt.java
@@ -54,8 +54,6 @@ import com.diffplug.common.collect.ImmutableList;
  */
 public class ConventionJdt extends OomphConvention {
 	final static String JDT_CORE_PREFS = ".metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.jdt.core.prefs";
-	final static List<String> JDT_COMPLIANCE_PROPS = ImmutableList.of("org.eclipse.jdt.core.compiler.codegen.targetPlatform", "org.eclipse.jdt.core.compiler.compliance", "org.eclipse.jdt.core.compiler.source");
-	final static String JDT_CLASSPATH_VAR_FMT = "org.eclipse.jdt.core.classpathVariable.%s";
 
 	ConventionJdt(OomphIdeExtension extension) {
 		super(extension);
@@ -64,8 +62,6 @@ public class ConventionJdt extends OomphConvention {
 	}
 
 	final Set<InstalledJre> installedJres = new HashSet<>();
-	final Map<String, String> classpathVariables = new LinkedHashMap<>();
-	private JavaVersion compilerComplianceLevel;
 
 	/** Adds an installed JRE with the given content. */
 	public void installedJre(Action<InstalledJre> action) {
@@ -76,12 +72,25 @@ public class ConventionJdt extends OomphConvention {
 
 	/** Sets default compliance level */
 	public void compilerComplianceLevel(String compilerComplianceLevel) {
-		this.compilerComplianceLevel = JavaVersion.toVersion(compilerComplianceLevel);
+		List<String> JDT_COMPLIANCE_PROPS = ImmutableList.of(
+				"org.eclipse.jdt.core.compiler.codegen.targetPlatform",
+				"org.eclipse.jdt.core.compiler.compliance",
+				"org.eclipse.jdt.core.compiler.source");
+		extension.workspaceProp(JDT_CORE_PREFS, props -> {
+			JDT_COMPLIANCE_PROPS.forEach(p -> props.put(p, compilerComplianceLevel.toString()));
+			//Use default compliance settings.
+			props.put("org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode", "enabled");
+			props.put("org.eclipse.jdt.core.compiler.problem.assertIdentifier", "error");
+			props.put("org.eclipse.jdt.core.compiler.problem.enumIdentifier", "error");
+		});
 	}
 
 	/** Adds a compiler class path variable. */
 	public void classpathVariable(String name, String value) {
-		classpathVariables.put(name, value);
+		String JDT_CLASSPATH_VAR_FMT = "org.eclipse.jdt.core.classpathVariable.%s";
+		extension.workspaceProp(JDT_CORE_PREFS, props -> {
+			props.put(String.format(JDT_CLASSPATH_VAR_FMT, name), value);
+		});
 	}
 
 	@Override
@@ -89,44 +98,6 @@ public class ConventionJdt extends OomphConvention {
 		// add installed jres
 		if (!installedJres.isEmpty()) {
 			extension.addSetupAction(new InstalledJreAdder(installedJres));
-		}
-		if (!classpathVariables.isEmpty()) {
-			extension.workspaceProp(JDT_CORE_PREFS, new JavaClasspathVariableAction(classpathVariables));
-		}
-		if (compilerComplianceLevel != null) {
-			extension.workspaceProp(JDT_CORE_PREFS, new JavaComplianceAction(compilerComplianceLevel));
-		}
-	}
-
-	static class JavaComplianceAction implements Action<Map<String, String>> {
-		private final JavaVersion complianceLevel;
-
-		public JavaComplianceAction(JavaVersion complianceLevel) {
-			super();
-			this.complianceLevel = complianceLevel;
-		}
-
-		@Override
-		public void execute(Map<String, String> props) {
-			JDT_COMPLIANCE_PROPS.forEach(p -> props.put(p, complianceLevel.toString()));
-			//Use default compliance settings.
-			props.put("org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode", "enabled");
-			props.put("org.eclipse.jdt.core.compiler.problem.assertIdentifier", "error");
-			props.put("org.eclipse.jdt.core.compiler.problem.enumIdentifier", "error");
-		}
-	}
-
-	static class JavaClasspathVariableAction implements Action<Map<String, String>> {
-		private final Map<String, String> classpathVariables;
-
-		public JavaClasspathVariableAction(Map<String, String> classpathVariables) {
-			super();
-			this.classpathVariables = classpathVariables;
-		}
-
-		@Override
-		public void execute(Map<String, String> props) {
-			classpathVariables.forEach((key, value) -> props.put(String.format(JDT_CLASSPATH_VAR_FMT, key), value));
 		}
 	}
 }

commit bf21861e89e50caad342043d4dcffdd30d5e9434

Fixed unused imports, and fixed a bug I introduced in 7434506f7fd3a069f899c1d535eaf0a876306c6f.

src/main/java/com/diffplug/gradle/oomph/ConventionJdt.java
@@ -16,9 +16,7 @@
 package com.diffplug.gradle.oomph;
 
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.gradle.api.Action;
@@ -77,7 +75,7 @@ public class ConventionJdt extends OomphConvention {
 				"org.eclipse.jdt.core.compiler.compliance",
 				"org.eclipse.jdt.core.compiler.source");
 		extension.workspaceProp(JDT_CORE_PREFS, props -> {
-			JDT_COMPLIANCE_PROPS.forEach(p -> props.put(p, compilerComplianceLevel.toString()));
+			JDT_COMPLIANCE_PROPS.forEach(p -> props.put(p, JavaVersion.toVersion(compilerComplianceLevel).toString()));
 			//Use default compliance settings.
 			props.put("org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode", "enabled");
 			props.put("org.eclipse.jdt.core.compiler.problem.assertIdentifier", "error");