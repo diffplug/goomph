4913f7652ae5d87973a459d5a00f9bce52a41409
restlessronin <88921269+restlessronin@users.noreply.github.com>
2022-06-26 01:00:24 +0530

    java 16+ classloader fix

--- src/main/java/com/diffplug/gradle/JRE.java
+++ src/main/java/com/diffplug/gradle/JRE.java
@@ -54,7 +54,14 @@ public class JRE {
 		} else {
 			// Assume AppClassLoader of Java9+
 			Class<? extends ClassLoader> clz = classLoader.getClass();
-			Field ucpFld = clz.getDeclaredField("ucp");
+			Field ucpFld;
+			try {
+				// Java 9 - 15
+				ucpFld = clz.getDeclaredField("ucp");
+			} catch (NoSuchFieldException e) {
+				// Java 16+
+				ucpFld = clz.getSuperclass().getDeclaredField("ucp");
+			}
 			ucpFld.setAccessible(true);
 			Object ucpObj = ucpFld.get(classLoader);
 			Field pathFld = ucpObj.getClass().getDeclaredField("path");
