f89c5c9c3e4f00d8e9d3e7c504de020c66522ccd
Ned Twigg <ned.twigg@diffplug.com>
2023-12-08 13:20:13 -0800

    Fix `com.diffplug.configuration-cache-for-platform-specific-build` (#214)

--- src/main/java/com/diffplug/gradle/swt/PlatformSpecificBuildPlugin.java
+++ src/main/java/com/diffplug/gradle/swt/PlatformSpecificBuildPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 DiffPlug
+ * Copyright (C) 2021-2023 DiffPlug
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,12 @@
  */
 package com.diffplug.gradle.swt;
 
-
 import com.diffplug.common.swt.os.OS;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.gradle.api.Plugin;
 import org.gradle.api.initialization.Settings;
+import org.gradle.api.provider.Provider;
 
 /**
  * In order to detect the underlying operating system and architecture, it is necessary to
@@ -29,10 +31,41 @@ import org.gradle.api.initialization.Settings;
  * the appropriate APIs</a> which don't break the configuration cache.
  */
 public class PlatformSpecificBuildPlugin implements Plugin<Settings> {
+
 	@Override
 	public void apply(Settings settings) {
 		OS.detectPlatform(
-				systemProp -> settings.getProviders().systemProperty(systemProp).forUseAtConfigurationTime().get(),
-				envVar -> settings.getProviders().environmentVariable(envVar).forUseAtConfigurationTime().get());
+				systemProp -> get(settings, settings.getProviders().systemProperty(systemProp)),
+				envVar -> get(settings, settings.getProviders().environmentVariable(envVar)),
+				cmds -> get(settings, settings.getProviders().exec(e -> {
+					e.commandLine(cmds.toArray());
+				}).getStandardOutput().getAsText()));
+	}
+
+	private <T> T get(Settings settings, Provider<T> provider) {
+		if (badSemver(settings.getGradle().getGradleVersion()) >= badSemver(STOP_FORUSE_AT_CONFIGURATION_TIME)) {
+			return provider.get();
+		} else {
+			return provider.forUseAtConfigurationTime().get();
+		}
+	}
+
+	static final String STOP_FORUSE_AT_CONFIGURATION_TIME = "7.4";
+
+	private static final Pattern BAD_SEMVER = Pattern.compile("(\\d+)\\.(\\d+)");
+
+	private static int badSemver(String input) {
+		Matcher matcher = BAD_SEMVER.matcher(input);
+		if (!matcher.find() || matcher.start() != 0) {
+			throw new IllegalArgumentException("Version must start with " + BAD_SEMVER.pattern());
+		}
+		String major = matcher.group(1);
+		String minor = matcher.group(2);
+		return badSemver(Integer.parseInt(major), Integer.parseInt(minor));
+	}
+
+	/** Ambiguous after 2147.483647.blah-blah */
+	private static int badSemver(int major, int minor) {
+		return major * 1_000_000 + minor;
 	}
 }
